9a65e5dc4768cf9fdab38ae00f2a2ddc756e9f3e
Omar Terrazas <terrazas.omar@gmail.com>
2020-04-15 00:53:05 -0500

    Axios has been integrated.

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios/index';
 
 class Chatbot extends Component {
     constructor(props) {
@@ -7,7 +8,41 @@ class Chatbot extends Component {
             messages: []
         };
     }
-    
+
+    async df_text_query(message) {
+        let says = {
+            speaks: 'me',
+            msg: {
+                text: {
+                    text: message
+                }
+            }
+        };
+
+        this.setState({messages: [...this.state.messages, says]});
+        const res = await axios.post('/api/df_text_query', {message});
+
+        for (let msg of res.data.fullfilmentMessages) {
+            says = {
+                speaks: 'bot',
+                msg: msg
+            }
+            this.setState({messages: [...this.state.messages, says]});
+        }
+    }
+
+    async df_event_query(event) {
+        const res = await axios.post('/api/df_event_query', {event});
+
+        for (let msg of res.data.fullfilmentMessages) {
+            says = {
+                speaks: 'me',
+                msg: msg
+            }
+            this.setState({messages: [...this.state.messages, says]});
+        }
+    }
+
     render () {
         return (
             <div style={{ height:400, width: 400, float: 'right' }}>
