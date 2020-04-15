fd04be9ce5d24b77a646b2f85671be16ef20edcc
Omar Terrazas <terrazas.omar@gmail.com>
2020-04-15 00:33:58 -0500

    Chatbot component has been created.

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Chatbot = () => (
-    <div style={{ background: '#DDD', padding: '10px' }}>
-        <p>Chatbot lives here</p>
-    </div>
-)
+class Chatbot extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            messages: []
+        };
+    }
+    
+    render () {
+        return (
+            <div style={{ height:400, width: 400, float: 'right' }}>
+                <div id="chatbot" style={{ height: '100%', width: '100%', overflow: 'auto' }}>
+                    <h2>Chatbot</h2>
+                    <div className="input-field">
+                        <input type="text" placeholder="Message" />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
 
 export default Chatbot;
